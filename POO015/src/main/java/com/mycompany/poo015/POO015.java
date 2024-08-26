5f47b24837a355959422fe6922d1445db62c28d2
Élber Borges <88691164+ElberBrgs@users.noreply.github.com>
2024-08-26 08:55:31 -0300

    poo - polimorfismo

--- POO015/src/main/java/com/mycompany/poo015/POO015.java
+++ POO015/src/main/java/com/mycompany/poo015/POO015.java
@@ -13,7 +13,10 @@ public class POO015 {
     public static void main(String[] args) {
         Cachorro bob = new Cachorro();
         Gato luna = new Gato();
+        Pato patolino = new Pato();
+        
         System.out.println("Chamando cachorro: "+bob.chamarAnimal()+"\nSom do cachorro: "+bob.emitirSom());
         System.out.println("Chamando gato: "+luna.chamarAnimal()+"\nSom do gato:  "+luna.emitirSom());
+        System.out.println("Chamando pato: "+patolino.chamarAnimal()+"\nSom do pato:  "+patolino.emitirSom());
     }
 }
